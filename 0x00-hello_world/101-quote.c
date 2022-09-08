6d37955b79c4bc81527780c99f4b276773e2a6e2
joshiebooming <joashgamba@gmail.com>
2022-09-08 14:03:11 -0700

    done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/*
+ * main -entry point 
+ * Return 0 (success)
+ */
+int main (void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",61);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+
+	return (1);
+}
+
