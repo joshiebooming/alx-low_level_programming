7284fd5d225d71638e9bebfdc85a3e47e3d1150d
joshiebooming <joashgamba@gmail.com>
2022-09-08 13:05:37 -0700

    commited

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 /*
  * main print the string in the put function
- * Description : using the main 
+ * Description using the main 
  * this program prints "Programming is like building a multilingual puzzle"
- * Return 0 
- */
-int main (void)
+ * Return 0  */
+int main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
 	return 0;
-
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,16 +6,15 @@
  * this program prints  prints the size of various types on the computer it is compiled and run on
  * Return 0
  */
-int main (void)
+int main(void)
 {
 	char c;
 	int i;
 	long int li;
 	long long int lli;
 	float f;
-
 	printf("size of a char: %ld byte(s)\n", sizeof(c));
-	printf("size of a int: %ld byte(s)\n", sizeof(i));
+	printf("size of an int: %ld byte(s)\n", sizeof(i));
 	printf("size of a long int: %ld byte(s)\n", sizeof(li));
 	printf("size of a long long int: %ld bytes(s)\n", sizeof(lli));
 	printf("size of a float: %ld bytes(s)\n",sizeof(f));
