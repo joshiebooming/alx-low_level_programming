d2ac8e59746024480baddc3a3055548b81e1bc56
joshiebooming <joashgamba@gmail.com>
2022-09-08 13:16:48 -0700

    add

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 /*
  * main - print the string  in the print function
- * Description - using the main function
+ *
+ * Description - 'using the main function'
+ * 
  *this program prints "with proper grammar, but the outcome is a piece of art,"
  *Return 0
  */
