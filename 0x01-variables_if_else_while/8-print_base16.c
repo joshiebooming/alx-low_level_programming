b8e8f47f379d9aac234e63e8317123e8b21dee1a
joshiebooming <joashgamba@gmail.com>
2022-09-11 11:05:40 -0700

    putchar

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,10 +8,10 @@
 int main(void)
 {
 	char c;
-	for (c ='0'; c <='9'; c++)
+	for (c ='0'; c <= '9'; c++)
 	{
 		putchar (c);
-		for (c = 'a'; c <='f'; c++)
+		for (c = 'a'; c <= 'f'; c++)
 			putchar (c);
 	}
  	putchar ('\n');
