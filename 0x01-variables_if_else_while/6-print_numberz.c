d8c373e81d29ab36ab7a5db1d4d9cec6fe6944f9
joshiebooming <joashgamba@gmail.com>
2022-09-11 11:33:05 -0700

    done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ * this cod e prints single digits  of base 10
+ * main function
+ * return 0
+ * prints all single digit numbers of base 10 starting from 0, followed by a new line.
+ */
+int main (void)
+{
+	int n;
+	for (n ='0'; n <='9'; n++)
+	{
+		putchar (n);
+
+	}
+	putchar ('\n');
+	return (0);
+}
+
+
