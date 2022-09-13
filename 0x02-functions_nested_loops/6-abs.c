f47b576c798b34d8dfc9df0bf50689755767bd3e
joshiebooming <joashgamba@gmail.com>
2022-09-13 07:25:31 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  *int _abs -  checks abolute value of interger
+  *@n - parameter getting checked
+  *return - 1,0 -1
+*/
+int _abs(int)
+{
+	if (n < 0)
+		n = -n;
+			return (n);
+}
