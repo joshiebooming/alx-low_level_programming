079647e022f60c4db41fbd4328118da25f889b28
joshiebooming <joashgamba@gmail.com>
2022-09-13 06:37:09 -0700

    4 alpha

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
-  *_islower-->checks if c is lowercase
-  *Return - Returns  1 or 0 depending on condition
-  * c - interger
-  * Return "1" or "0"
- **/
+  *_islower - checks if c is lowercase
+  *@c - interger to be checked
+  * Return - "1" or "0" depending on condition
+ */
 int _islower(int c)
 {
 	return (c >= 'a' && c <= 'z');
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,11 @@
 #include "main.h"
 /**
   * _isalpha - checks if c is a letter lower case or upper case
-  * @c is a character 
-  * Return - either 1 or 0 depending on condition
+  * @c: is a character
+  * Return: - either or 0 depending on condition
 */
 
 int _isalpha(int c)
 {
 	return ((c > 'a' && c <= 'z') || (c > 'A' && c <= 'Z'));
-	
 }
