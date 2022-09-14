3f6379834126251b111854a25e3ca36f7cd4417a
joshiebooming <joashgamba@gmail.com>
2022-09-14 11:54:49 -0700

    fix

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 /**
   * main - checks sum of even fib
-  *Return : 0
+  *Return:is  0
 */
-int main (void)
+int main(void)
 {
-	unsigned long num1 = 0, num2 = 1, num3 = 0, sum =0;
+	unsigned long num1 = 0, num2 = 1, num3 = 0, sum = 0;
+
 	while (num3 < 400000)
 	{
 		num3 = num1 + num2;
 		num1 = num2;
 		num2  = num3;
 	if ((num1 % 2) == 0)
-		sum +=num1;
+		sum += num1;
 	}
 		printf("%ld\n", sum);
 		return (0);
