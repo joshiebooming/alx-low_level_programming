a84256294ef47edf0f06143107ee030e879e4d3b
joshiebooming <joashgamba@gmail.com>
2022-09-13 22:15:18 -0700

    fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,14 +13,14 @@ for (n = 0; n <= 9; n++)
 	for  (mult = 1; mult <= 9; mult++)
 	{
 		_putchar(',');
-		_putchat(' ');
+		_putchar(' ');
 		prodct = n * mult;
 		if (prodct <= 9)
 		
 			_putchar(' ');
 		else
 			_putchar((prodct / 10) + '0');
-			_putchar((prod % 10) +'0');
+			_putchar((prodct % 10) +'0');
 		
 	}
 	_putchar('\n');
