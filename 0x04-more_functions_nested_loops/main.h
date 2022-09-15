b2b2c942a56cfe031307d22c8281b60bf41b6060
joshiebooming <joashgamba@gmail.com>
2022-09-15 11:05:06 -0700

    push

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -9,4 +9,13 @@
 
 int _isupper(int);
 int _isdigit(int);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+int _putchar(char);
+
 #endif /*MAIN_H */
