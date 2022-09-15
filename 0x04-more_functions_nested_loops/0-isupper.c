5371c479e21f42af8aef4572c9b000d7b88005b8
joshiebooming <joashgamba@gmail.com>
2022-09-15 09:19:20 -0700

    dep

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*_isupper - check if c is upper case
+*@c: parameter getting checked
+* Return 0 or 1 on condition
+*/
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z')
+}
