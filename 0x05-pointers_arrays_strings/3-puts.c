4a44ed321b26329e70381b1a75da2adc48a5db48
joshiebooming <joashgamba@gmail.com>
2022-09-19 13:16:03 -0700

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _puts - main function that puts
+*@str - parameter
+*Return: 0
+*/
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n');
+}
