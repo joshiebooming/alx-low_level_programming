7418933a3783ad93fb8e1cc3fac773f5c0e5a314
joshiebooming <joashgamba@gmail.com>
2022-09-19 13:32:20 -0700

    fixed

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,12 +6,12 @@
 */
 void print_rev(char *s)
 {
-	int i,n;
+	int i, n;
 
 	n = 0;
 	while (s[n] != '\0')
 		n++;
-	for (i = n-1; i >= 0; i--)
+	for (i = n - 1; i >= 0; i--)
 	{
 		_putchar(s[i]);
 	}
