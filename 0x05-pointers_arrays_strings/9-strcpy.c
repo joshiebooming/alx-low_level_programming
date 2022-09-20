7288dca86a96adfefb5657107b614958849638c5
joshiebooming <joashgamba@gmail.com>
2022-09-19 19:37:31 -0700

    betty fix

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -3,19 +3,19 @@
 /**
 *_strcpy - copy paste string
 *@dest: destination parameter
-*@src: source parameter 
+*@src: source parameter
+*Return: destination
 */
 
 char *_strcpy(char *dest, char *src)
 {
 	int inc = 0;
-	
+
 	while (*(src + inc) != '\0')
 	{
 		*(dest + inc) = *(src + inc);
 		inc++;
 	}
 		*(dest + inc) = '\0';
-	
 		return (dest);
 }
