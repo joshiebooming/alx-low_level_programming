24520d12aff773a70b6ca655e034a8d912a4ffc4
joshiebooming <joashgamba@gmail.com>
2022-09-19 18:50:30 -0700

    10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+* _atoi -converts string to int
+*@s: the string to be changed
+* Return: converted string
+*/
+
+int _atoi(char *s)
+{
+	int sign = 1;
+	unsigned int num = 0;
+
+	do {
+	if (*s == '-')
+		sign *= -1;
+	else if (*s >= '0' && *s <= '9')
+		num = num * 10 + (*s - '0');
+	else if (num > 0)
+		break;
+	} while (*s++);
+		return (num * sign);
+}
