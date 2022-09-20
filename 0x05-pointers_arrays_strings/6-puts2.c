8041d2af394edbb1da3600e3bbaed82d12ea08c3
joshiebooming <joashgamba@gmail.com>
2022-09-19 18:22:39 -0700

    string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+*puts2 -prints out the characters
+*@str: stringg
+*Return: 0
+*/
+
+void puts2(char *str)
+{
+	int i = 0
+	
+		while (*(str+1) != '\0')
+		{
+			if ( 1 % 2 == 0)
+				putchar(*(str + 1));
+			i++;
+		}
+		putchar(10);
+}
+
