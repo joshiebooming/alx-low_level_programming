d55a39ffa2a22601d059287705af6afe6ab6484a
joshiebooming <joashgamba@gmail.com>
2022-09-23 06:13:32 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*_strncat-concatanates two strings
+*@dest - this is the string to append on
+*@src: string to be completed at end of dest
+*@n : this the parameter we will compare the index to
+*Return: returns new concatenated string
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int ind = 0, dest_ln = 0;
+	
+	while (dest[ind++])
+		dest_ln++;
+	for (ind = 0; src[ind] && ind < n; ind++)
+		dest[dest_ln++] = src[ind];
+	return (dest);
+}
+
+
