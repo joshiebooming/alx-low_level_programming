236cfefa8c8017ea73983b74a377b192562da394
joshiebooming <joashgamba@gmail.com>
2022-09-26 10:26:08 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*_strchr -locates characters in a string
+*@s: this is a string to check
+*@c: this is the character to be checked for 
+*Return:pointer to spot in s with c or null
+*/
+char *_strchr(char *s, char c)
+{
+	int j;
+	
+	for (j = 0; *(s + j); j++)
+	{
+		if (*(s + j) == c)
+			return (s  + j);
+	}
+	if (*(s  + j) == c)
+		return (s + j);
+	return (0);
+}
