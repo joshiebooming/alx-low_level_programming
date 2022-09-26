7e907cae323ae5ac7e582f14ca68b5a09483f6b5
joshiebooming <joashgamba@gmail.com>
2022-09-26 10:32:22 -0700

    hot fix

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+  *void prototyes()
+  * int prototypes()
+  */
+
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -9,3 +16,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
+#endif /*MAIN_H*/
