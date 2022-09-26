61a2bce9de4bc89cbf4d41bbdebc7bdc73f6ccfa
joshiebooming <joashgamba@gmail.com>
2022-09-26 10:35:18 -0700

    fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -3,13 +3,13 @@
 /**
 *_strchr -locates characters in a string
 *@s: this is a string to check
-*@c: this is the character to be checked for 
+*@c: this is the character to be checked for
 *Return:pointer to spot in s with c or null
 */
 char *_strchr(char *s, char c)
 {
 	int j;
-	
+
 	for (j = 0; *(s + j); j++)
 	{
 		if (*(s + j) == c)
