ffc01b232f1e512bab3da2eb6c0355a1aea0fa30
joshiebooming <joashgamba@gmail.com>
2022-09-28 08:13:06 -0700

    recursions

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*
+* _puts_recursion - prints string folowed by new line
+*@s : string to be called
+*Return : void null
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar ('\n');
+}
+	else 
+{
+		_putchar(s [0] );
+		_puts_recursion(s + 1);
+}
+	
+
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget - prints a string, followed by a new line.
