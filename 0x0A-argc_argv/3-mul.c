cfdd61686b89c4853810f01bf4e0b9794322be0f
joshiebooming <joashgamba@gmail.com>
2022-09-30 14:15:49 -0700

    fix

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
 
 /*
- * main - function passs arguements
+ *main- main function passs arguements
  *argc : counts arguements
  *argv: the arguements
  * return : 0 otherwise 1
 */
 int main(int argc, char *argv[])
 {
-	int a, int b;
-	
-	if ( argc == 3)
+	int a, b;
+
+	if (argc == 3)
 	{
 		a = atoi(argv[1]);
 		b = atoi(argv[2]);
-	printf("%d\n",a *b);
+	printf("%d\n", a * b);
 	return (0);
 	}
 	printf("error\n");
