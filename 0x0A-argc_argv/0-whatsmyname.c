0f25179d77c84661a66da592c45402dbda1f05be
joshiebooming <joashgamba@gmail.com>
2022-09-30 14:00:39 -0700

    fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 
 /**
-*main -
-*argc: void
-*argv:prints string of arguement 
-* return :0  otherwise return 1
+*main -function
+*@argc : arguement
+*@argv:prints string of arguement
+*Return: 0  otherwise return 1
 */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	(void) argc;
 
-	printf ("%s\n", argv[0]);
+	printf("%s\n", argv[0]);
 	return (0);
 }
