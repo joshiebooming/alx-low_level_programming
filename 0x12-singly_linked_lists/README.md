002c9a4bb7d83e7a472b60d0619963962adc7e84
joshiebooming <joashgamba@gmail.com>
2022-10-25 12:18:50 -0700

    commit

---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,7 @@
+0. Print list - function that prints all the elements of a list list.
+1. List length - function that returns the number of elements in a linked list_t list.
+2. Add node -  function that adds a new node at the beginning of a list_t list.
+3. Add node at the end -  function that adds a new node at the end of a list_t list.
+4. Free list -  function that frees a list_t list
+5. The Hare and the Tortoise - function that prints You're beat! and yet, you must allow,\nI bore my house upon my back!\n before the main
+6. Real programmers can write assembly code in any language
---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,27 @@
+#ifndef  LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+*struct list_s  - singly linked lists
+*@str: string  (mallocd string
+*@len : length of string
+*@next : points to next node
+*Description: singly linked node structure 
+*/
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+int _strlen(const char *str);
+
+#endif /*LISTS_H*/
