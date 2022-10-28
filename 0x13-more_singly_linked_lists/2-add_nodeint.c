9c46dd426fd72de4056e41e2d265c4c8e4639e05
joshiebooming <joashgamba@gmail.com>
2022-10-28 02:38:09 -0700

    fixng

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -6,9 +6,9 @@
  *@head:double pointer to the beginning
  *Return: address of new node
  */
-listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listsint_t *new;
+	listint_t *new;
 
 	if (head == NULL)
 		return (NULL);
--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -6,7 +6,7 @@
  *@head: pointer to beginning of list
  *Return: void
  */
-void free_listint(listint_t *head);
+void free_listint(listint_t *head)
 {
 	listint_t *next;
 
